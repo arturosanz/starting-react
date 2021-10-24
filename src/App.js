44b55145511f67ac1c5ab46d31b1cb77d9c432b6
Arturo Sanz <arturo@post.harvard.edu>
2021-10-24 09:07:04 +0200

    Episode #6

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import pokemon from "./pokemon.json";
 import PropTypes from "prop-types";
 
 const PokemonRow = ({ pokemon, onSelect }) => (
@@ -56,6 +55,12 @@ PokemonInfo.propTypes = {
 function App() {
 	const [filter, setFilter] = React.useState("");
 	const [selectedItem, setSelectedItem] = React.useState(null);
+	const [pokemon, setPokemon] = React.useState([]);
+	React.useEffect(() => {
+		fetch("http://localhost:3000/starting-react/pokemon.json")
+			.then((resp) => resp.json())
+			.then((data) => setPokemon(data));
+	}, []);
 	return (
 		<div
 			style={{

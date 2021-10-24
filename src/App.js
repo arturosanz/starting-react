4d348028b7f565d2132c6c39e60eaa06746f885a
Arturo Sanz <arturo@post.harvard.edu>
2021-10-24 12:10:43 +0200

    Episodes #7 to #9

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./App.css";
 import PropTypes from "prop-types";
+import Button from "@mui/material/Button";
 
 const PokemonRow = ({ pokemon, onSelect }) => (
 	<tr>
 		<td>{pokemon.name.english}</td>
 		<td>{pokemon.type.join(", ")}</td>
 		<td>
-			<button onClick={() => onSelect(pokemon)}>Select!</button>
+			<Button
+				variant="outlined"
+				size="small"
+				color="secondary"
+				onClick={() => onSelect(pokemon)}
+			>
+				Select!
+			</Button>
 		</td>
 	</tr>
 );
